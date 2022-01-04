b5958432c2e135dab7457177260972a23bdd0bd2
gonnatinfo2000 <58569152+gonnatinfo2000@users.noreply.github.com>
2022-01-04 15:38:30 +0000

    Update teste.java

--- teste.java
+++ teste.java
@@ -2,18 +2,13 @@ import java.util.Scanner;
 
 public class Teste {
 
-    public static int soma(int num1, int num2){
-        int soma = num1 + num2;
-        return soma;
-    }
-
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite um primeiro número: ");
         int num1 = scanner.nextInt();
         System.out.println("Digite um segundo número: ");
         int num2 = scanner.nextInt();
-        int soma_numeros = soma(num1, num2);
+        int soma_numeros = num1 + num2;
         System.out.println("Soma:" + soma_numeros);
     }
 
